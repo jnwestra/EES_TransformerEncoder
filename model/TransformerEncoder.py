b706dddfc4147ce4c7bb146fc0b7fce6d84f68e0
jnwestra <jnwestra1695@gmail.com>
2021-05-11 05:01:52 -0400

    Update TransformerEncoder.py

--- model/TransformerEncoder.py
+++ model/TransformerEncoder.py
@@ -8,7 +8,7 @@ class TransformerEncoder(nn.Module):
 
     def __init__(self, input_size, hidden_size, n_layer, ff_size=2048, n_head=16, dropout=0.2):
 
-        super(TransformerEncoder, self).__init__()
+        #super(TransformerEncoder, self).__init__()
 		
         self.input_size = input_size
         self.hidden_size = hidden_size
