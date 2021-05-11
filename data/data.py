eee147c9f6c5b4d8dea2e5dc02093b2d8d2221d2
jnwestra <jnwestra1695@gmail.com>
2021-05-11 13:26:19 -0400

    Update data.py

--- data/data.py
+++ data/data.py
@@ -11,22 +11,21 @@ class ImgDmDataset(Dataset):
     def __init__(self, split: str, path: str) -> None:
         assert split in ['train', 'val', 'test']
         self._data_path = join(path, split)
-        self._n_data = _count_data(self._data_path)
-        self._prev_js = None
+        self._names, self._n_data = _get_names(self._data_path)
 
     def __len__(self) -> int:
         return self._n_data
 
     def __getitem__(self, i: int):
-        with open(join(self._data_path, '{}.json'.format(i))) as f:
+        with open(join(self._data_path, self._names[i])) as f:
             js = json.loads(f.read())
         return js
 
 
-def _count_data(path):
+def _get_names(path):
     """ count number of data in the given path"""
     matcher = re.compile(r'[0-9]+\.json')
     match = lambda name: bool(matcher.match(name))
     names = os.listdir(path)
     n_data = len(list(filter(match, names)))
-    return n_data
+    return names, n_data
