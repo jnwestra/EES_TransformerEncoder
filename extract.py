b03d49506a39a95406778d20a7038608e82af804
jnwestra <jnwestra1695@gmail.com>
2021-05-12 17:12:25 -0400

    Update extract.py

--- extract.py
+++ extract.py
@@ -215,7 +215,7 @@ class SummarizerEncoder(nn.Module):
             for s, n in zip(enc_sents, sent_nums)],
             dim=0
             )
-        print(enc_sent.size(0),enc_sent.size(1))
+        print(input_len.size(),input_len.eq(0.0).unsqueeze(1))
         batch_size, seq_len = enc_sent.size(0), enc_sent.size(1)
 
         # prepare mask
