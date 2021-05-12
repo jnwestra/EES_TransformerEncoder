a7ae32d3da4ae3b0c1300b565a69df50cf9c6ed4
jnwestra <jnwestra1695@gmail.com>
2021-05-12 14:30:59 -0400

    Update run.py

--- run.py
+++ run.py
@@ -142,8 +142,7 @@ def get_encoded(args, split):
         log_file.write(f'Getting encoded article sentences at {str(timedelta(seconds=start%86400))}')
         with torch.no_grad():
             for raw_article_batch in loader:
-                tokenized_article_batch = map(tokenize(None), raw_article_batch)
-                log_file.write(f'{str(tokenized_article_batch)}\n')
+                tokenized_article_batch = map(tokenize(None,raw_article_batch), raw_article_batch)
                 for raw_art_sents in tokenized_article_batch:
                     enc_out = encoder(raw_art_sents)
                     enc_list.append(enc_out)
