708c53d9a4d271b6cc0f76d77f03c5c4b1087244
jnwestra <jnwestra1695@gmail.com>
2021-05-12 15:13:04 -0400

    Update data.py

--- data/data.py
+++ data/data.py
@@ -10,7 +10,7 @@ from torch.utils.data import Dataset
 class ImgDmDataset(Dataset):
     def __init__(self, path: str, log_file: io.TextIOWrapper) -> None:
         self._data_path = path
-        for filename in os.listdir(path) log_file.write(f'{filename}\n')
+        log_file.write(f'{os.listdir(path)}\n')
         self._names, self._n_data = list_data(self._data_path)
         for name in self._names log_file.write(f'{name}\n')
 
